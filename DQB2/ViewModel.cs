b9830bd678a9833fdaf1e6660eaee4ebb30ffabc
turtle-insect <turtle.insect@gmail.com>
2020-02-28 09:53:07 +0900

    add bag param

--- DQB2/ViewModel.cs
+++ DQB2/ViewModel.cs
@@ -9,14 +9,20 @@ namespace DQB2
 {
 	class ViewModel
 	{
-		public ObservableCollection<Item> Inventory { get; set; } = new ObservableCollection<Item>();
 		public Player Player { get; set; } = new Player();
+		public ObservableCollection<Item> Inventory { get; set; } = new ObservableCollection<Item>();
+		public ObservableCollection<Item> Bag { get; set; } = new ObservableCollection<Item>();
 
 		public ViewModel()
 		{
 			for (uint i = 0; i < 15; i++)
 			{
 				Inventory.Add(new Item(0x55B28D + i * 4));
+			}
+
+			for (uint i = 0; i < 420; i++)
+			{
+				Bag.Add(new Item(0x55B2C9 + i * 4));
 			}
 		}
 	}

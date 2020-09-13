38831572510483241b467eadeaf8a11743eaded0
turtle-insect <turtle.insect@gmail.com>
2020-09-14 00:02:29 +0900

    add builder heart

---
+++ STGDAT/ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STGDAT
+{
+	class ViewModel
+	{
+		public uint Heart
+		{
+			get { return SaveData.Instance().ReadNumber(0xC0ECC, 4); }
+			set { Util.WriteNumber(0xC0ECC, 4, value, 0, 99999); }
+		}
+	}
+}

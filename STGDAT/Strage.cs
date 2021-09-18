1a293e4a4d9b72e3262af19d652f0bdcadca3ff8
turtle-insect <turtle.insect@gmail.com>
2021-09-18 10:52:06 +0900

    refactor

--- STGDAT/Strage.cs
+++ STGDAT/Strage.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ComponentModel;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 
 namespace STGDAT
 {
